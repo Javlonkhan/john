c2c3acb5e429feca6b08f47a2ece070da82d356f
Javlonkhan <khamija6251_aehit@students.vizja.pl>
2024-10-09 17:08:45 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,25 @@
+line1 = "*******************"
+line2 = "****JAvlonkhan*****"
+line3 = "****JAVLONKHAN!****"
+
+print('')
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
+print('"Nexia", "TICO", \'Damas\' ko\'rganlar qilar havas')
+print(5**4)
+print(22%4)
+print(125*125, 4*125)
+print(3.14*(12/2)**2)
+print((6**2+7**2)**(1/2))
+kocha = "Bog'bon"
+mahalla = "Sog'bon"
+tuman = "bodomzor"
+viloyat = "Samarqand"
+print(kocha + ' ' + "ko'chasi," +  mahalla + ' ' + "mahallasi," + viloyat + ' ' + "viloyati ."  )
+k_m_t_v = f"{kocha} ko'chasi, {mahalla} mahallasi {tuman} tumani {viloyat} viloyati"
+print(k_m_t_v)
+ona = "mother"
+print('ona')
